b5d9a9a7cf4c4b07b31b4a3d3a501899b1ece47c
Mayur-Murarka <137669111+Mayur-Murarka@users.noreply.github.com>
2025-01-05 16:12:40 +0530

    change in navbar

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -102,7 +102,7 @@ const Navbar = () => {
             </SheetTrigger>
             <SheetContent>
               <SheetHeader>
-                <SheetTitle className="font-bold my-4">HarryBlog</SheetTitle>
+                <SheetTitle className="font-bold my-4">MayurBlog</SheetTitle>
                 <SheetDescription>
                   <div className="flex flex-col gap-6">
                     <Link href="/"> Home</Link>
