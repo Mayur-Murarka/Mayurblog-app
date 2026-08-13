a896ec54fb6a97e3a775c7717b6c098c4512e8f1
Mayur-Murarka <137669111+Mayur-Murarka@users.noreply.github.com>
2026-08-13 21:23:40 +0530

    test(isolate): replace components/ui/Footer.js + components/ui/theme-provider.js with placeholders to isolate client error

--- components/ui/theme-provider.js
+++ components/ui/theme-provider.js
@@ -1,11 +1,6 @@
-"use client"
+"use client";
+import React from "react";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-
-export function ThemeProvider({
-  children,
-  ...props
-}) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export default function themeproviderPlaceholder() {
+  return (<div data-placeholder="themeprovider">Placeholder: themeprovider</div>);
 }
